8e10f2e3f77f3f7fa0014bd1486a9756161be45a
JulienLach <leadedchamps@gmail.com>
2024-01-09 13:53:28 +0100

    ok

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// Chargement du DOM avant execution du code 
+document.addEventListener('DOMContentLoaded', () => {
+    let board = null // initialiser le plateau
+    const jeu = new Chess(); // Créer une nouvelle instance Chess.js
+    const historiqueMouvements = document.getElementById("historique-mouvements") // attraper la div de historique mouvements
+    let compteur
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
